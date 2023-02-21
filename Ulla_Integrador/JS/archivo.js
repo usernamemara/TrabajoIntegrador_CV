34802932966a21de5c3efbc7c2dbbe6f71376ec9
usernamemara <marasoleu@gmail.com>
2023-02-21 16:25:52 -0300

    Primer Entrega, Argentina Programa 4.0 -- CV de persona ficticia.

---
+++ Ulla_Integrador/JS/archivo.js
@@ -0,0 +1,32 @@
+if(document.getElementById("btnContacto")){
+    var modal = document.getElementById("ventana_modal");
+    var btn = document.getElementById("btnContacto");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
---
+++ Ulla_Integrador/README.txt
@@ -0,0 +1,5 @@
+Proyecto Final Integrador, Argentina Programa 4.0. Febrero de 2023.
+
+Enunciado: solicitan que realices una página web que posea un curriculum vitae. 
+
+Nota: el ejemplo entregado es el CV de una persona ficticia.
